12bc32e3a9d32dbd757607f5eac668379f90edd9
chrisIyer <hellohotbox420@gmail.com>
2024-08-12 14:21:10 -0400

    folders

---
+++ src/app/dashboard/pageone/page.js
@@ -0,0 +1,4 @@
+export default function SettingsPage() {
+     return <h1>User Settings</h1>
+   }
+   
---
+++ src/app/dashboard/pagetwo/page.js
@@ -0,0 +1,5 @@
+// Inside /dashboard/settings/page.js
+export default function PageTwo() {
+     return <h1>Two</h1>
+   }
+   

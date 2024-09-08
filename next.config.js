e26fd9acb31cdd31673fceee23946360c519bffd
chrisIyer <hellohotbox420@gmail.com>
2024-09-08 14:59:07 -0400

    liss

--- next.config.js
+++ next.config.js
@@ -2,12 +2,22 @@
 module.exports = {
   images: {
     remotePatterns: [
+      
       {
+        protocol: 'https',
+        hostname: 'commons.wikimedia.org',
+        port: '', // Use your local development port
+        pathname: '/**', // Use '/**' to allow all paths under the hostname
+      },
+
+      {
+
         protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '', // Leave empty if no specific port is required
         pathname: '/**', // Use '**' to allow all paths under the hostname
       },
+      
     ],
   },
 };

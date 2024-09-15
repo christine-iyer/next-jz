b6aab3ab8c97c0e5e04ab8919ba762afa343022c
chrisIyer <hellohotbox420@gmail.com>
2024-09-15 18:59:04 -0400

    tailwined installed sucessfully. Now I have to learn it.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Add your JSX and TSX paths here
-    './public/index.html',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}', // If you're using the /app directory in Next.js 13
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
-

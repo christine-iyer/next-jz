8264b92de6616d79c8cabb147a31ad25f42f5f12
chrisIyer <hellohotbox420@gmail.com>
2024-09-28 17:41:21 -0400

    added electorate data

--- app/(dashboard)/pagefive/page.tsx
+++ app/(dashboard)/pagefive/page.tsx
@@ -58,6 +58,59 @@ const statePopulation = {
   'Wyoming': 578759
 
 };
+const stateElectorate = {
+  'Alabama': 9,
+  'Alaska': 3,
+  'Arizona': 11,
+  'Arkansas': 6,
+  'California': 54,
+  'Colorado': 10,
+  'Connecticut': 7,
+  'Delaware': 3,
+  'District': 3,
+  'Florida': 30,
+  'Georgia': 16,
+  'Hawaii': 4,
+  'Idaho': 4,
+  'Illinois': 19,
+  'Indiana': 11,
+  'Iowa': 6,
+  'Kansas': 6,
+  'Kentucky': 8,
+  'Louisiana': 8,
+  'Maine': 4,
+  'Maryland': 10,
+  'Massachusetts': 11,
+  'Michigan': 15,
+  'Minnesota': 10,
+  'Mississippi': 6,
+  'Missouri': 10,
+  'Montana': 4,
+  'Nebraska': 5,
+  'Nevada': 6,
+  'New Hampshire': 4,
+'New Jersey': 14,
+'New Mexico': 5,
+'New York': 28,
+'North Carolina': 16,
+'North Dakota': 3,
+'Ohio': 17,
+  'Oklahoma': 7,
+  'Oregon': 8,
+  'Pennsylvania': 19,
+  'Rhode Island': 4,
+'South Carolina': 9,
+'South Dakota': 3,
+'Tennessee': 11,
+  'Texas': 40,
+  'Utah': 6,
+  'Vermont': 3,
+  'Virginia': 13,
+  'Washington': 12,
+  'West Virginia': 4,
+'Wisconsin': 10,
+  'Wyoming': 3
+}
 
 
 const Map = () => {
@@ -74,7 +127,7 @@ const Map = () => {
     const path = d3.geoPath().projection(projection);
 
     // Color scale for population (light to dark)
-    const populationValues = Object.values(statePopulation);
+    const populationValues = Object.values(stateElectorate);
     const colorScale = d3.scaleLinear()
       .domain([d3.min(populationValues), d3.max(populationValues)])
       .range(["#e41bd0", "#9416e9"]);
@@ -99,14 +152,14 @@ const Map = () => {
       .attr("d", path)
       .attr("fill", (d) => {
         const stateName = d.properties.NAME;
-        const population = statePopulation[stateName] || 0;
+        const population = stateElectorate[stateName] || 0;
         return colorScale(population);
       })
       .attr("stroke", "#a45b8e")
       .attr("stroke-width", 1.5)
       .on("mouseover", function (event, d) {
         const stateName = d.properties.NAME;
-        const population = statePopulation[stateName] || "Unknown";
+        const population = stateElectorate[stateName] || "Unknown";
 
         tooltip
           .transition()
